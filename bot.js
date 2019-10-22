679b6e9de6bc2f843eff1be0acdcc9ea8ad99708
sugarcube <auztin410@gmail.com>
2019-10-21 20:30:04 -0400

    Searching for item based on command.

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const magicItems = require('./MagicItemsList.json');
+
+var found = null;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -9,8 +12,18 @@ client.on('ready', () => {
 client.login(auth.token);
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (msg.content === '!ping') {
       msg.reply('pong');
     }
   });
-  
+
+  client.on('message', msg => {
+    var item = msg.content.substring(6);
+    found = magicItems.filter(function(magicItem) {
+      return item == magicItem.Name;
+    });
+    if (msg.content.substring(0,5) === '!item' && found != undefined) {
+      msg.reply(found);
+      console.log(found);
+    }
+  });
